0a1686cbd08bb7f37c5294f324f0855ade54f754
sayedyunus <88835384+sayedyunus@users.noreply.github.com>
2021-08-24 16:10:30 +0530

    Add files via upload

---
+++ assign_one/q2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{   int a[15];
+    int num=0;
+    int j=0;
+    printf("enter any 15 numbers\n");
+    for (int i = 0; i < 15; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    while (j<15)
+    
+    {
+            if (num<=a[j])
+            {
+                num=a[j];
+                j++;
+            }
+            else
+            j++;
+            
+    }
+
+    printf("greatest number is %d",num);
+    return 0;
+    
+}
